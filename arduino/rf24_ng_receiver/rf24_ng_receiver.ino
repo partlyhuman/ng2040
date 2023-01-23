002849ab92eb9cfc375f95be3eea4ebae7774adf
Roger Braunstein <roger@partlyhuman.com>
2023-01-23 10:21:12 -0800

    Changing receiver strategy to always send as well

--- arduino/rf24_ng_receiver/rf24_ng_receiver.ino
+++ arduino/rf24_ng_receiver/rf24_ng_receiver.ino
@@ -3,13 +3,16 @@
 #include "RF24.h"
 
 #undef DEBUG
+#define USE_IRQ
 #define SENDER 0
 
-#define DEBUGLED(t) digitalWrite(16, t)
+#define DEBUGLED(t)
 
 #define RADIO_IRQ_PIN 27
 #define RADIO_CE_PIN 26
 #define RADIO_CS_PIN 13
+#define RADIO_LEVEL RF24_PA_MIN
+#define RADIO_CHANNEL 119
 
 RF24 radio(RADIO_CE_PIN, RADIO_CS_PIN);
 
@@ -17,20 +20,9 @@ typedef uint16_t payload_t;
 
 #define JOY_PIN_COUNT 10
 
+//                                    L  R  D  U  A  B  C  D  SEL STA
 uint8_t joyOutPins[JOY_PIN_COUNT] = { 2, 3, 1, 0, 4, 5, 6, 7,  9,  8 };
-
-
-// Let these addresses be used for the pair
 uint8_t address[][6] = { "P1", "P2", "P3", "P4", "P5", "P6" };
-// It is very helpful to think of an address as a path instead of as
-// an identifying device destination
-
-// to use different addresses on a pair of radios, we need a variable to
-// uniquely identify which address this radio will use to transmit
-
-// For this example, we'll be using a payload containing
-// a single float number that will be incremented
-// on every successful transmission
 static payload_t payload = 0;
 static payload_t lastPayload = 0;
 
@@ -43,26 +35,17 @@ void rxInterrupt() {
 
   if (radio.available()) {
     radio.read(&payload, sizeof(payload));
-  }
 
-  if (payload == lastPayload) {
-    return;
-  }
-
-  lastPayload = payload;
-  for (int i = 0; i < JOY_PIN_COUNT; i++) {
-    bool pressed = !(payload & (1 << i));
-    pinMode(joyOutPins[i], pressed ? OUTPUT : INPUT);
-    //    if (pressed) digitalWrite(joyOutPins[i], LOW);
-    //    digitalWrite(joyOutPins[i], pressed ? LOW : HIGH);
-  }
-
+    for (int i = 0; i < JOY_PIN_COUNT; i++) {
+      bool pressed = !(payload & (1 << i));
+      pinMode(joyOutPins[i], pressed ? OUTPUT : INPUT);
+    }
 
 #ifdef DEBUG
-  //  radio.printDetails();
-  Serial.println(payload, BIN);
+    //  radio.printDetails();
+    Serial.println(payload, BIN);
 #endif
-
+  }
 }
 
 void setup() {
@@ -95,45 +78,29 @@ void setup() {
   }
 
 
-  // Set the PA Level low to try preventing power supply related problems
-  // because these examples are likely run with nodes in close proximity to
-  // each other. MIN/LOW/HIGH/MAX
-  radio.setPALevel(RF24_PA_MIN);  // RF24_PA_MAX is default.
-
-  radio.setChannel(24);
-  //  radio.setDataRate(RF24_250KBPS); // RF24_1MBPS RF24_2MBPS RF24_250KBPS
-  //  radio.setRetries(2, 10);
-
-
-  // save on transmission time by setting the radio to only transmit the
-  // number of bytes we need to transmit a float
+  radio.setPALevel(RADIO_LEVEL);
+  radio.setChannel(RADIO_CHANNEL);
+  radio.setDataRate(RF24_1MBPS); // RF24_1MBPS RF24_2MBPS RF24_250KBPS
   radio.setPayloadSize(sizeof(payload_t));
-
   radio.setAddressWidth(3);
-  //  radio.setAutoAck(false);
-  //  radio.disableAckPayload();
-  //  radio.disableDynamicPayloads();
+  radio.setAutoAck(false);
+  radio.disableDynamicPayloads();
 
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[SENDER]);  // always uses pipe 0
+  radio.openWritingPipe(address[SENDER]);
+  radio.openReadingPipe(1, address[!SENDER]);
+  radio.startListening();
 
-  // set the RX address of the TX node into a RX pipe
-  radio.openReadingPipe(1, address[!SENDER]);  // using pipe 1
-
-  // additional setup specific to the node's role
+#ifdef USE_IRQ
   radio.maskIRQ(1, 1, 0);  // args = "data_sent", "data_fail", "data_ready
-  radio.startListening();  // put radio in RX mode
-
   pinMode(RADIO_IRQ_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(RADIO_IRQ_PIN), rxInterrupt, FALLING);
+#endif
 
   for (int i = 0; i < JOY_PIN_COUNT; i++) {
-    digitalWrite(joyOutPins[i], LOW);
     pinMode(joyOutPins[i], INPUT);
   }
 
 #ifdef DEBUG
-  // For debugging info
   printf_begin();             // needed only once for printing details
   radio.printDetails();       // (smaller) function that prints raw register values
   radio.printPrettyDetails(); // (larger) function that prints human readable data
@@ -142,4 +109,17 @@ void setup() {
 }
 
 void loop() {
+#ifndef USE_IRQ
+  if (radio.available()) {
+    radio.read(&payload, sizeof(payload_t));
+#ifdef DEBUG
+    Serial.println(payload);  // print the payload's value
+#endif
+    for (int i = 0; i < JOY_PIN_COUNT; i++) {
+      bool pressed = !(payload & (1 << i));
+      pinMode(joyOutPins[i], pressed ? OUTPUT : INPUT);
+      // digitalWrite(joyOutPins[i], LOW);
+    }
+  }
+#endif
 }
